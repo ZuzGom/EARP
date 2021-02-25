f3ddf1f65f1428090390ca7d8fcd5784cad18215
ZuzGom <zuzgom@gmail.com>
2021-02-25 11:23:00 +0100

    AAAAAAAAAA

--- bat.py
+++ bat.py
@@ -2,11 +2,15 @@ from getpass import getpass
 from mysql.connector import connect, Error
 
 try:
+    print('lets go')
     with connect(
         host="ekonomik.atthost24.pl",
+        name="18013_earp",
         user="18013_earp",
         password=getpass("earp.123"),
     ) as connection:
+        print('bro')
         print(connection)
 except Error as e:
     print(e)
+print('no')
