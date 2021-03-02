2fcb71226c64e67deaa5e9881fb595e689aa5933
ZuzGom <zuzgom@gmail.com>
2021-03-02 18:54:50 +0100

    duzo plikow

--- main.py
+++ main.py
@@ -2,20 +2,30 @@ import os
 os.environ['KIVY_GL_BACKEND'] = 'sdl2'
 from kivy.core.window import Window
 import kivy
+from kivy.uix.image import Image
+from kivy.uix.widget import Widget
+from kivy.uix.label import Label
 from kivy.app import App
 from kivy.lang import Builder
+#from kivy.properties import ObjectProperty, ListProperty
 import kivy_garden.contextmenu
 
+Builder.load_file('simple.kv')
+#Window.clearcolor = (249, 201, 1, 1)
+'''
+class Tlo(Label):
+    background = ListProperty((0.2, 0.2, 0.2))
+'''
+
+class Warstwa(Widget):
+    pass
 
 class MyApp(App):
     def build(self):
-        self.title = 'Simple context menu example'
-        return Builder.load_file('menu.kv')
-
-    def say_hello(self, text):
-        print(text)
-        self.root.ids['context_menu'].hide()
-
+        self.title = 'EARP'
+        
+        Window.clearcolor = (250/255,233/255,203/255,1)
+        return Warstwa()
 
 if __name__ == '__main__':
     MyApp().run()
