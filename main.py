2cd0e8471f94807c8915afef62fabc5618feae4a
ZuzGom <zuzgom@gmail.com>
2021-03-05 21:12:05 +0100

    try except

--- main.py
+++ main.py
@@ -3,19 +3,26 @@ kivy.require('1.0.7')
 from kivy.core.window import Window
 from kivy.app import App
 from kivy.uix.widget import Widget
-import kivy_garden.contextmenu
-from plyer import notification
+#from kivy.uix.label import Label
+#from kivy.uix.popup import Popup
 
-class Warstwa(Widget):
-    def idle(self):
-        notification.notify(title='Twój ul jest bezpiczny!', message='poprzez Elektorniczny Asystent Rodziny Pszczelej')
-    pass
-class TestApp(App):
-    Window.clearcolor = (250/255,233/255,203/255,1)
-    def build(self):
-        return Warstwa()
+try:
+    import kivy_garden.contextmenu
+    from plyer import notification
 
-# buildozer android debug deploy run
+    class Warstwa(Widget):
+        def idle(self):
+            notification.notify(title='Twój ul jest bezpiczny!', message='poprzez Elektorniczny Asystent Rodziny Pszczelej')
+        pass
+    class TestApp(App):
+        Window.clearcolor = (250/255,233/255,203/255,1)
+        def build(self):
+            return Warstwa()
 
-if __name__ == '__main__':
-    TestApp().run()
+    # buildozer android debug deploy run
+    if __name__ == '__main__':
+        TestApp().run()
+except Exception as ex:
+    err = '{}: {})'.format(ex.__class__.__name__, ex)
+    notification.notify(title='Coś poszło nie tak ;(', message=err, timeout=20)
+    
