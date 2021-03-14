f9f5fbd96f18638089f3d9f26cf4cfe33ab94b65
ZuzGom <zuzgom@gmail.com>
2021-03-14 22:58:07 +0100

    v2

--- main.py
+++ main.py
@@ -1,32 +1,67 @@
-import kivy
-kivy.require('1.0.7')
-from kivy.core.window import Window
 from kivy.app import App
+from kivy.lang import Builder
 from kivy.uix.widget import Widget
 from kivy.uix.image import Image
-#from kivy.uix.label import Label
-#from kivy.uix.popup import Popup
+from kivy.core.window import Window
+from kivy.uix.button import Button
+from kivy.uix.dropdown import DropDown
+from kivy.uix.floatlayout import FloatLayout
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.relativelayout import RelativeLayout
+from kivy.uix.screenmanager import ScreenManager, Screen
+from kivy_garden.contextmenu import ContextMenu, ContextMenuItem
+from plyer import notification
 
-try:
-    import kivy_garden.contextmenu
-    from plyer import notification
-    class Warstwa(Widget):
-        def idle(self):
-            notification.notify(title='Twój ul jest bezpiczny!', message='poprzez Elektorniczny Asystent Rodziny Pszczelej')
-        pass
-    class TestApp(App):
-        Window.clearcolor = (250/255,233/255,203/255,1)
-        #Window.clearcolor = (40/255,40/255,40/255,1)
-        def build(self):
-            #self.theme_cls.primary_palette = "Yellow"
-            #self.theme_cls.primary_hue = "100"
-            #self.theme_cls.theme_style = "Dark"
-            return Warstwa()
+# Create both screens. Please note the root.manager.current: this is how
+# you can control the ScreenManager from kv. Each screen has by default a
+# property manager that gives you the instance of the ScreenManager used.
 
-    # buildozer android debug deploy run
-    if __name__ == '__main__':
-        TestApp().run()
-except Exception as ex:
-    err = '{}: {})'.format(ex.__class__.__name__, ex)
-    notification.notify(title='Coś poszło nie tak ;(', message=err, timeout=20)
+#root = root()
+#root = Widget()
+def Manag(scr):
+    scr.add_widget(MenuScreen(name="menu"))
+    scr.add_widget(Ule(name="ule"))
+    scr.add_widget(Alert(name="alert"))
+    scr.add_widget(Notif(name="arch"))
+
+global MenuScreen, Ule, Alert, sm
+
+sm = ScreenManager()
+
+class Menu(Widget):
+    @staticmethod   
+    def now(name):
+        sm.current = name
+
+class MenuScreen(Screen):
+    pass
     
+
+class SettingsScreen(Screen):
+    pass
+class Ule(Screen):
+    pass
+class Alert(Screen):
+    def idle(self):
+        notification.notify(title='Twój ul jest bezpiczny!', 
+        message='poprzez Elektorniczny Asystent Rodziny Pszczelej')
+    
+class Notif(Screen):
+    pass
+
+#sm = ScreenManager()
+#Scr.add_widget(MenuScreen(name="menu"))
+
+class TestApp(App):
+    Window.clearcolor = (40/255,40/255,40/255,1)
+    def build(self):
+        #sm = ScreenManager()
+        Manag(sm)
+        return sm
+        
+    
+
+        
+
+if __name__ == '__main__':
+    TestApp().run()
