6778199bca74dd335ae9b32c6a5c8c229ab1816f
ZuzGom <zuzgom@gmail.com>
2021-03-20 23:43:45 +0100

    lokin good

--- main.py
+++ main.py
@@ -1,16 +1,13 @@
 from kivy.app import App
 from kivy.lang import Builder
 from kivy.uix.widget import Widget
-from kivy.uix.image import Image
 from kivy.core.window import Window
-from kivy.uix.button import Button
-from kivy.uix.dropdown import DropDown
+from kivy.uix.scrollview import ScrollView
 from kivy.uix.floatlayout import FloatLayout
-from kivy.uix.boxlayout import BoxLayout
-from kivy.uix.relativelayout import RelativeLayout
+from kivy.uix.dropdown import DropDown
 from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy_garden.contextmenu import ContextMenu, ContextMenuItem
-from plyer import notification
+from plyer import notification, email
 
 # Create both screens. Please note the root.manager.current: this is how
 # you can control the ScreenManager from kv. Each screen has by default a
@@ -19,23 +16,27 @@ from plyer import notification
 #root = root()
 #root = Widget()
 def Manag(scr):
-    scr.add_widget(MenuScreen(name="menu"))
+    #scr.add_widget(MenuScreen(name="menu"))
     scr.add_widget(Ule(name="ule"))
     scr.add_widget(Alert(name="alert"))
     scr.add_widget(Notif(name="arch"))
+    scr.add_widget(Sett(name="set"))
 
 global MenuScreen, Ule, Alert, sm
 
 sm = ScreenManager()
 
-class Menu(Widget):
+class Menu(FloatLayout):
     @staticmethod   
     def now(name):
         sm.current = name
 
-class MenuScreen(Screen):
-    pass
-    
+class Sett(Screen):
+    def checkbox_click(self, instance, value): 
+        if value is True: 
+            Window.clearcolor = (40/255,40/255,40/255,1) 
+        else: 
+            Window.clearcolor = (250/255,233/255,203/255,1)
 
 class SettingsScreen(Screen):
     pass
@@ -60,8 +61,15 @@ class TestApp(App):
         return sm
         
     
-
+#buildozer android debug deploy run
         
+try:
+    if __name__ == '__main__':
+        TestApp().run()
+except Exception as ex:
+    
+    err = '{}: {})'.format(ex.__class__.__name__, ex)
+    notification.notify(title='Coś poszło nie tak ;(', message=err, timeout=20)
+    notification.notify(title='Prosimy o wysłanie maila z błędem', message='Dziękujemy za współpracę', timeout=20)
+    email.send(recipient='zuzgom@gmail.com', subject ='Error', text=ex, create_chooser=True)
 
-if __name__ == '__main__':
-    TestApp().run()
