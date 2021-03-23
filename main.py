e96f61df5b60ed8efc4ff9b3c2e6e6406221d9bf
ZuzGom <74205927+ZuzGom@users.noreply.github.com>
2021-03-23 13:08:01 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,73 +1,80 @@
 from kivy.app import App
-from kivy.lang import Builder
-from kivy.uix.widget import Widget
 from kivy.core.window import Window
-from kivy.uix.scrollview import ScrollView
 from kivy.uix.floatlayout import FloatLayout
-from kivy.uix.dropdown import DropDown
 from kivy.uix.screenmanager import ScreenManager, Screen
-from kivy_garden.contextmenu import ContextMenu, ContextMenuItem
-from plyer import notification, email
+from plyer import notification
+
 
 # Create both screens. Please note the root.manager.current: this is how
 # you can control the ScreenManager from kv. Each screen has by default a
 # property manager that gives you the instance of the ScreenManager used.
 
-#root = root()
-#root = Widget()
-def Manag(scr):
-    #scr.add_widget(MenuScreen(name="menu"))
+# root = root()
+# root = Widget()
+def manag(scr):
+    # scr.add_widget(MenuScreen(name="menu"))
     scr.add_widget(Ule(name="ule"))
     scr.add_widget(Alert(name="alert"))
     scr.add_widget(Notif(name="arch"))
     scr.add_widget(Sett(name="set"))
 
+
 global MenuScreen, Ule, Alert, sm
 
 sm = ScreenManager()
 
+
 class Menu(FloatLayout):
-    @staticmethod   
+    @staticmethod
     def now(name):
         sm.current = name
 
+
 class Sett(Screen):
-    def checkbox_click(self, instance, value): 
-        if value is True: 
-            Window.clearcolor = (40/255,40/255,40/255,1) 
-        else: 
-            Window.clearcolor = (250/255,233/255,203/255,1)
+    @staticmethod
+    def checkbox_click(value):
+        if value is True:
+            Window.clearcolor = (40 / 255, 40 / 255, 40 / 255, 1)
+        else:
+            Window.clearcolor = (250 / 255, 233 / 255, 203 / 255, 1)
+
 
 class SettingsScreen(Screen):
     pass
+
+
 class Ule(Screen):
     pass
+
+
 class Alert(Screen):
     pass
-    
+
+
 class Notif(Screen):
     pass
 
-#sm = ScreenManager()
-#Scr.add_widget(MenuScreen(name="menu"))
+
+# sm = ScreenManager()
+# Scr.add_widget(MenuScreen(name="menu"))
 
 class TestApp(App):
-    Window.clearcolor = (40/255,40/255,40/255,1)
+    Window.clearcolor = (40 / 255, 40 / 255, 40 / 255, 1)
+
     def build(self):
-        #sm = ScreenManager()
-        Manag(sm)
+        # sm = ScreenManager()
+        manag(sm)
         return sm
-        
-    
-#buildozer android debug deploy run
-        
+
+
+# buildozer android debug deploy run
+
 try:
     if __name__ == '__main__':
         TestApp().run()
 except Exception as ex:
-    
+
     err = '{}: {})'.format(ex.__class__.__name__, ex)
     notification.notify(title='Coś poszło nie tak ;(', message=err, timeout=20)
     notification.notify(title='Prosimy o wysłanie maila z błędem', message='Dziękujemy za współpracę', timeout=20)
-    #email.send(recipient='zuzgom@gmail.com', subject ='Error', text=ex, create_chooser=True)
-
+    # email.send(recipient='zuzgom@gmail.com', subject ='Error', text=ex, create_chooser=True)
